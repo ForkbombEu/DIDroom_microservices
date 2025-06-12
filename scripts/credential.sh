070b845063ad83b9e9f38624054247d2df380bb4
matteo-cristino <cristino.matteo97@gmail.com>
2025-06-12 10:20:22 +0200

    test(wk): update claims and format to draft 15
    add also ldp_vc example to credential_configurations_supported

--- scripts/credential.sh
+++ scripts/credential.sh
@@ -9,12 +9,14 @@ if [ ! -d ${CUSTOM_CODE} ] || [ ! -f ${WELL_KNOWN} ]; then
 fi
 
 # get all vct from well_known
-vcts=$(jq -r '.credential_configurations_supported.[].vct' ${WELL_KNOWN})
+# vcts=$(jq -r '.credential_configurations_supported.[].vct' ${WELL_KNOWN})
+vcts=$(jq -r '.credential_configurations_supported | to_entries[] | select(.value.format == "vc+sd-jwt") | .value.vct' "${WELL_KNOWN}")
+ldp_keys=$(jq -r '.credential_configurations_supported | to_entries[] | select(.value.format == "ldp_vc") | .key' "${WELL_KNOWN}")
 
 chain="  "
 for vct in ${vcts}; do
 cat <<EOF >> ${CHAIN}
-  - id: Custom $vct
+  - id: Custom vc+sd-jwt $vct
     precondition:
       zencode: |
         Given I have a 'string' named 'vct'
@@ -25,7 +27,7 @@ cat <<EOF >> ${CHAIN}
     zencodeFromFile: credential_issuer/custom_code/$vct.zen
     keysFromFile: credential_issuer/custom_code/$vct.keys.json
     dataFromStep: Introspection
-  - id: sd_jwt
+  - id: sd_jwt for $vct
     precondition:
       zencode: |
         Given I have a 'string' named 'vct'
@@ -38,4 +40,29 @@ cat <<EOF >> ${CHAIN}
     dataFromStep: Custom $vct
 EOF
 done
-
+for ldp_key in ${ldp_keys}; do
+cat <<EOF >> ${CHAIN}
+  - id: Custom ldp_vc $ldp_keys
+    precondition:
+      zencode: |
+        Given I have a 'string' named 'vct'
+        When I set 'condition' to '$vct' as 'string'
+        When I verify 'vct' is equal to 'condition'
+        Then print the string '$vct'
+      dataFromStep: Verify
+    zencodeFromFile: credential_issuer/custom_code/$vct.zen
+    keysFromFile: credential_issuer/custom_code/$vct.keys.json
+    dataFromStep: Introspection
+  - id: w3c_vc for $ldp_keys
+    precondition:
+      zencode: |
+        Given I have a 'string' named 'vct'
+        When I set 'condition' to '$vct' as 'string'
+        When I verify 'vct' is equal to 'condition'
+        Then print the string '$vct'
+      dataFromStep: Verify
+    zencodeFromFile: credential_issuer/credential_4_sd_jwt.zencode
+    keysFromStep: Verify
+    dataFromStep: Custom $vct
+EOF
+done
