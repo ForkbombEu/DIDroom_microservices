dc5d3cd29c403ee096673af6a671a1886635bcd3
matteo-cristino <cristino.matteo97@gmail.com>
2025-11-12 10:13:27 +0100

    fix: use new @slangroom/dcql plusing for vp_token validation

--- scripts/verifier.sh
+++ scripts/verifier.sh
@@ -19,66 +19,39 @@ cat <<EOF > "${CHAIN}"
 steps:
   - id: transaction_id_path
     zencodeFromFile: verifier/transaction_id_path.zencode
-  - id: verify_dc+sd-jwt
-    zencodeFromFile: verifier/verify_dc+sd-jwt.zencode
-    keysFromFile: verifier/verify_dc+sd-jwt.keys.json
+  - id: verify_vptoken
+    zencodeFromFile: verifier/verify_vptoken.zencode
     dataFromStep: transaction_id_path
-    precondition:
-      zencode: |
-        Given I have a 'string' named 'precondition_format'
-        When I set 'dc+sd-jwt' to 'dc+sd-jwt' as 'string'
-        When I verify 'precondition_format' is equal to 'dc+sd-jwt'
-        Then print the data
-      dataFromStep: transaction_id_path
     onError:
       zencodeFromFile: verifier/error.zencode
       dataFromStep: transaction_id_path
       keys:
-        error: Cryptographic verification of dc+sd-jwt failed
-  - id: verify_ldp_vc
-    zencodeFromFile: verifier/verify_ldp_vc.zencode
-    keysFromFile: verifier/verify_ldp_vc.keys.json
-    dataFromStep: transaction_id_path
-    precondition:
-      zencode: |
-        Given I have a 'string' named 'precondition_format'
-        When I set 'ldp_vc' to 'ldp_vc' as 'string'
-        When I verify 'precondition_format' is equal to 'ldp_vc'
-        Then print the data
-      dataFromStep: transaction_id_path
-    onError:
-      zencodeFromFile: verifier/error.zencode
-      dataFromStep: transaction_id_path
-      keys:
-        error: Cryptographic verification of ldp_vc failed
+        error: Cryptographic verification failed
 EOF
 
 for file in "${files[@]}"; do
     basefile=$(basename "${file}" .zen)
 cat <<EOF >> "${CHAIN}"
-  - id: custom_code_${basefile}_dc+sd-jwt
+  - id: custom_code_${basefile}
     zencodeFromFile: verifier/custom_code/$basefile.zen
     keysFromFile: verifier/custom_code/$basefile.keys.json
-    dataFromStep: verify_dc+sd-jwt
+    dataFromStep: verify_vptoken
     precondition:
       zencode: |
         Given I have a 'string' named 'precondition_type'
-        Given I have a 'string' named 'precondition_format'
         When I set '$basefile' to '$basefile' as 'string'
         When I verify 'precondition_type' is equal to '$basefile'
-        When I set 'dc+sd-jwt' to 'dc+sd-jwt' as 'string'
-        When I verify 'precondition_format' is equal to 'dc+sd-jwt'
         Then print the data
       dataFromStep: transaction_id_path
     onError:
       zencodeFromFile: verifier/error.zencode
       dataFromStep: transaction_id_path
       keys:
-        error: Custom code verification $basefile dc+sd-jwt failed
-  - id: save_result_to_file_${basefile}_dc+sd-jwt
+        error: Custom code verification $basefile failed
+  - id: save_result_to_file_${basefile}
     zencodeFromFile: verifier/save_result_to_file.zencode
-    keysFromStep: verify_dc+sd-jwt
-    dataFromStep: custom_code_${basefile}_dc+sd-jwt
+    keysFromStep: verify_vptoken
+    dataFromStep: custom_code_${basefile}
     dataTransform: |
       const d = JSON.parse(data);
       const r = {custom_result: d};
@@ -86,48 +59,8 @@ cat <<EOF >> "${CHAIN}"
     precondition:
       zencode: |
         Given I have a 'string' named 'precondition_type'
-        Given I have a 'string' named 'precondition_format'
         When I set '$basefile' to '$basefile' as 'string'
         When I verify 'precondition_type' is equal to '$basefile'
-        When I set 'dc+sd-jwt' to 'dc+sd-jwt' as 'string'
-        When I verify 'precondition_format' is equal to 'dc+sd-jwt'
-        Then print the data
-      dataFromStep: transaction_id_path
-  - id: custom_code_${basefile}_ldp_vc
-    zencodeFromFile: verifier/custom_code/$basefile.zen
-    keysFromFile: verifier/custom_code/$basefile.keys.json
-    dataFromStep: verify_ldp_vc
-    precondition:
-      zencode: |
-        Given I have a 'string' named 'precondition_type'
-        Given I have a 'string' named 'precondition_format'
-        When I set '$basefile' to '$basefile' as 'string'
-        When I verify 'precondition_type' is equal to '$basefile'
-        When I set 'ldp_vc' to 'ldp_vc' as 'string'
-        When I verify 'precondition_format' is equal to 'ldp_vc'
-        Then print the data
-      dataFromStep: transaction_id_path
-    onError:
-      zencodeFromFile: verifier/error.zencode
-      dataFromStep: transaction_id_path
-      keys:
-        error: Custom code verification $basefile ldp_vc failed
-  - id: save_result_to_file_${basefile}_ldp_vc
-    zencodeFromFile: verifier/save_result_to_file.zencode
-    keysFromStep: verify_ldp_vc
-    dataFromStep: custom_code_${basefile}_ldp_vc
-    dataTransform: |
-      const d = JSON.parse(data);
-      const r = {custom_result: d};
-      return JSON.stringify(r);
-    precondition:
-      zencode: |
-        Given I have a 'string' named 'precondition_type'
-        Given I have a 'string' named 'precondition_format'
-        When I set '$basefile' to '$basefile' as 'string'
-        When I verify 'precondition_type' is equal to '$basefile'
-        When I set 'ldp_vc' to 'ldp_vc' as 'string'
-        When I verify 'precondition_format' is equal to 'ldp_vc'
         Then print the data
       dataFromStep: transaction_id_path
 EOF
